ad61b18be53f4075a67311ea94a5f00c44e43abf
doloiEoh <do.loi@eoh.io>
2024-08-16 16:54:29 +0700

    fix code

--- App/Containers/RoomDetail/index..js
+++ App/Containers/RoomDetail/index..js
@@ -4,7 +4,6 @@ import {
   FlatList,
   Image,
   KeyboardAvoidingView,
-  StyleSheet,
   TextInput,
   View,
 } from 'react-native';
@@ -16,6 +15,7 @@ import {axiosGet} from '../../Apis/axios';
 import API from '../../Configs/API';
 import moment from 'moment';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import styles from './styles';
 
 const RoomDetail = () => {
   const route = useRoute();
@@ -111,60 +111,5 @@ const RoomDetail = () => {
     </KeyboardAvoidingView>
   );
 };
-
-const styles = StyleSheet.create({
-  messageContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 5,
-  },
-  background: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    padding: 20,
-    marginBottom: 10,
-  },
-  myMessage: {
-    alignSelf: 'flex-end',
-    margin: 10,
-    padding: 10,
-    backgroundColor: '#DCF8C6',
-    borderRadius: 20,
-    maxWidth: '80%',
-  },
-  otherMessage: {
-    alignSelf: 'flex-start',
-    margin: 10,
-    padding: 10,
-    backgroundColor: '#FFFFFF',
-    borderRadius: 20,
-    maxWidth: '80%',
-  },
-  avatarStyle: {
-    width: 40,
-    height: 40,
-    borderRadius: 20,
-    marginRight: 10,
-  },
-  textName: {
-    fontWeight: 600,
-    marginBottom: 5,
-    fontSize: 12,
-  },
-  input: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 10,
-  },
-  iconContainer: {
-    marginRight: 4,
-  },
-  timestamp: {
-    fontSize: 12,
-    color: '#888',
-    marginTop: 5,
-  },
-});
 
 export default RoomDetail;
--- App/Containers/RoomDetail/styles.js
+++ App/Containers/RoomDetail/styles.js
@@ -2,22 +2,56 @@ import {StyleSheet} from 'react-native';
 import {Colors} from '../../Configs/Colors';
 
 export default StyleSheet.create({
-  container: {
+  messageContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 5,
+  },
+  background: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: Colors.Gray,
+    justifyContent: 'flex-end',
+    padding: 20,
+    marginBottom: 10,
   },
-  loadingContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
+  myMessage: {
+    alignSelf: 'flex-end',
+    margin: 10,
+    padding: 10,
+    backgroundColor: '#DCF8C6',
+    borderRadius: 20,
+    maxWidth: '80%',
   },
-  icon: {
-    position: 'absolute',
-    justifyContent: 'center',
-    alignItems: 'center',
+  otherMessage: {
+    alignSelf: 'flex-start',
+    margin: 10,
+    padding: 10,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 20,
+    maxWidth: '80%',
   },
-  logo: {
-    zIndex: -1,
+  avatarStyle: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    marginRight: 10,
+  },
+  textName: {
+    fontWeight: 600,
+    marginBottom: 5,
+    fontSize: 12,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+  },
+  iconContainer: {
+    marginRight: 4,
+  },
+  timestamp: {
+    fontSize: 12,
+    color: '#888',
+    marginTop: 5,
   },
 });
